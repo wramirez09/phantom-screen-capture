485827a50d19ad5e109002e580e0b34dfe8aa3ad
Ramirez William <william.ramirez@designory.com>
2017-02-22 15:59:25 -0600

    - phantom set up part 2

--- api/api.js
+++ api/api.js
@@ -3,40 +3,41 @@ var app = express();
 var phantom = require('phantom');
 
 
-module.exports.post = function(req, res) {
+module.exports.postIndex = function(req, res) {
+    console.log('postIndex')
+}
 
-    console.log("query", req.query)
-        /* req.query is an object containing all url parameters */
+module.exports.phantomscreencapture = function(req, res) {
+
+    console.log("query", req.query);
+
+    /* req.query is an object containing all url parameters */
+
+    // var viewport = {
+    //     width: req.query.width,
+    //     height: req.query.height
+    // };
+
+    // var defaulted = 0;
 
 
-    var viewport = {
-        width: req.query.width,
-        height: req.query.height
-    };
-
-    var clipRect = {
-        top: 0,
-        left: 0,
-        width: viewport.width,
-        height: viewport.height
-    };
-
+    // // default
+    // var clipRect = {
+    //     top: defaulted,
+    //     left: defaulted,
+    //     width: viewport.width,
+    //     height: viewport.height
+    // };
 
     phantom.create().then(function(ph) {
 
         ph.createPage().then(function(page) {
 
-            // page.viewportSize = viewport;
-
-            // page.clipRect = clipRect;
-
             page.open(req.query.url);
 
-            page.render("screenshots/screenshot2.png");
+            page.render(req.query.url + ".png");
 
-            ph.exit();
-        });
+            // ph.exit();
+        })
     });
-
-
 }
--- server.js
+++ server.js
@@ -2,22 +2,15 @@ var express = require('express');
 var app = express();
 var api = require('./api/api');
 
-
-
-function callback(req, res) {
-    console.log("index page hit");
-};
-
-
 app.use(express.static('public'));
 
-// index page end point
-app.all('/', callback);
+/* console.log on post only */
 
 // phantom-capture end point
-app.all("/phantom-capture/", api.post.bind(this));
+app.all("/phantom-capture/", api.phantomscreencapture.bind(this));
 
+app.all("/", api.postIndex.bind(this));
 // app listen
-app.listen(3100, function() {
+app.listen(3100, function(req, res) {
     console.log('app listening on port 3100!')
 });
