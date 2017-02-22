a2b3050f294d3c23fbf4410a02dadaab6682c937
Ramirez William <william.ramirez@designory.com>
2017-02-21 20:22:24 -0600

    - install phantom

--- api/api.js
+++ api/api.js
@@ -5,10 +5,38 @@ var phantom = require('phantom');
 
 module.exports.post = function(req, res) {
 
-
-
     console.log("query", req.query)
-        /* req.query */
+        /* req.query is an object containing all url parameters */
+
+
+    var viewport = {
+        width: req.query.width,
+        height: req.query.height
+    };
+
+    var clipRect = {
+        top: 0,
+        left: 0,
+        width: viewport.width,
+        height: viewport.height
+    };
+
+
+    phantom.create().then(function(ph) {
+
+        ph.createPage().then(function(page) {
+
+            // page.viewportSize = viewport;
+
+            // page.clipRect = clipRect;
+
+            page.open(req.query.url);
+
+            page.render("screenshots/screenshot2.png");
+
+            ph.exit();
+        });
+    });
 
 
 }
