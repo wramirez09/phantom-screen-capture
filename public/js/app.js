99ac7b2e48018ea518411d558734e6552f1ceec5
Ramirez William <william.ramirez@designory.com>
2017-07-19 11:31:58 -0500

    replace2

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,8 @@ app.ui = function() {
     var formselector = ".phantom-form",
         $form = $(formselector),
         $overlay = ".overlay",
-        $overlayImg = '.overlay-img'
+        $overlayImg = '.overlay-img',
+        $imageCont = $('#imageCont');
 
     var formgroup = ".form-group";
     var isFocusClass = "is-focused"
@@ -12,55 +13,121 @@ app.ui = function() {
     var args = {
         'isFocusedClass': "is-focused",
         "formgroup": ".form-group",
-        "phantomforminput": ".phantom-form :input",
-        "isMobile": false
+        "phantomforminput": ".phantom-form :input, textarea.multipleurls",
+        "mobileshot": false,
+        "multipleshot": false,
+        "crawler": false
     }
 
     function init(args) {
-        // turn on ui 
-        toggleUI(args.formgroup, args.isFocusedClass, args);
         // handle form submission 
         $form.submit(function(event) {
+
             event.preventDefault();
+
             showLoader($overlay, $overlayImg);
             var $inputs = $(args.phantomforminput);
-            values = {};
-            $inputs.each(function() {
-                values[this.name] = $(this).val();
-            });
-            if (args.isMobile) {
+            var values = {};
+            var screenWidth = 320,
+                screenHeight = 568,
+                multipleshot = args.multipleshot;
+
+            if (args.mobileshot == true) {
+
+
+                /* mobile config - hard coded iphone 5 */
                 userAgentString = 'Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>';
+
                 screenWidth = 320;
                 screenHeight = 568;
+
+
             } else {
-                var userAgentString = navigator.userAgent,
-                    screenWidth = window.screen.width,
-                    screenHeight = window.screen.height;
+                /* desktop config values pulled from the clinet  */
+                var userAgentString = navigator.userAgent;
+                screenWidth = window.screen.width;
+                screenHeight = window.screen.height;
+
             }
+
             values['userAgent'] = userAgentString;
             values["screenWidth"] = screenWidth;
             values["screenHeight"] = screenHeight;
+            values["multipleshot"] = multipleshot;
+            values["mobileshot"] = args.mobileshot;
+            values["crawler"] = args.crawler;
+
+            $inputs.each(function() {
+                values[this.name] = $(this).val();
+            });
+
+            console.log("values", values)
+
             submitForm(values);
         });
+
     }
+
 
     $("#isMobile").on("change", function() {
         if ($(this).is(":checked")) {
-            args.isMobile = true;
-            console.log("ismobile value set", args.isMobile)
+            args.mobileshot = true;
+            console.log("args.isMobile", args.mobileshot)
+        } else {
+            args.mobileshot = false;
+            console.log("args.isMobile", args.mobileshot)
         }
+    })
+
+    function slideToggle(radiobutton, thisEL, thatEL, isSingle) {
+        if (isSingle) {
+            $(radiobutton).attr("checked", false);
+            $(thisEL).slideDown();
+            $(thatEL).slideUp();
+        } else {
+            $(radiobutton).attr("checked", false);
+            $(thisEL).slideDown();
+            $(thatEL).slideUp();
+        }
+    }
+
+    $("#isMultiple").on("change", function() {
+        args.multipleshot = true;
+        if ($(this).is(":checked")) {
+            console.log(args.multipleshot)
+            slideToggle('#isSingle', ".multipleurls", ".singleinput", true);
+        } else {}
+
+    })
+
+    $("#isSingle").on("change", function() {
+        args.multipleshot = false;
+        if ($(this).is(":checked")) {
+            console.log(args.multipleshot)
+            slideToggle("#isMultiple", ".singleinput", ".multipleurls", true);
+        } else {}
+
+    })
+
+    $("#cawler").on("change", function() {
+        args.multipleshot = false;
+        if ($(this).is(":checked")) {
+            args.crawler = 'true';
+            console.log('crawler', args.crawler)
+                // slideToggle("#isMultiple", ".singleinput", ".multipleurls", true);
+        } else {
+            args.crawler = 'false';
+            console.log('crawler', args.crawler)
+        }
+
     })
     init(args);
 
-    function toggleUI(el, toggleClass, args) {
-        $(el).on("click", function() {
-            $(this).toggleClass(toggleClass);
-        });
-    }
 
     function submitForm($data) {
         $.ajax({
-            url: "http://localhost:3100/phantom-capture/",
+            url: "http://10.88.24.26:9000/phantom-capture/",
+            // url: "http://localhost:9000/phantom-capture/",
             data: $data,
             success: function(returnedData) {
                 successHandler(returnedData)
@@ -85,28 +152,58 @@ app.ui = function() {
         console.log('returnedData', returnedData);
         var result = "";
         for (var key in returnedData) {
-            result += "<b >" + key + "</b> " + returnedData[key] + "<br />";
+            result += "<b><a href='#' class='key'>" + key + "</a> </b>" + "<a href='#'>" + returnedData[key] + "</a>";
         }
 
         $(".data").empty();
         $(".data").append(result)
-    }
+    };
 
-    function successHandler(returnedData) {
-        console.log('success', returnedData);
+    function printImage(returnedData) {
         $("#imageCont").find("img").remove();
 
         setTimeout(function() {
-            var $previewImg = $('.card-image img').remove();
+            $('#imgCont > img').remove();
             var newImage = new Image();
             newImage.src = "/screenshots/" + returnedData.filename + "." + returnedData.fileTypeExtension;
+            // hide loader 
             showLoader($overlay, $overlayImg, 'hide');
-            $(".card-image").append(newImage);
+            $("#imgCont").append(newImage).addClass("thumbnail center-block").hide("fast").fadeIn("easeIn");
+            // add url to img for DL
             document.getElementById("downloadBtn").href = newImage.src;
+            // print returned object
             showData(returnedData);
-        }, 1000);
+        }, 500);
+    };
+
+    function printmultimessage(returnedData) {
+        console.log("multi shot data returned");
+        document.getElementById("downloadBtn").href = returnedData.zippath;
+        setTimeout(function() {
+                showLoader($overlay, $overlayImg, 'hide');
+                var $previewImg = $('#imgCont > img').remove();
+                $("#imgCont").html('<h2>your zip file is ready for downloading</h2>').hide("fast").fadeIn("easeIn");
+            },
+            500);
+        showData(returnedData);
+    };
+
+    function successHandler(returnedData) {
+        console.log('success', returnedData);
+
+        if (returnedData.multi == "true") {
+            printmultimessage(returnedData);
+        } else {
+            printImage(returnedData);
+        }
     }
 };
+
+
+$(".trigger").on("click", function() {
+    $(this).toggleClass('active');
+    $(this).next(".inputCont").slideToggle("ease");
+})
 
 
 $(document).ready(function() {
